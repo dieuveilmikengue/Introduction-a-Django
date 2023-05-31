b1d32fd34f5be39495e52c8b76416d7fdc62b867
Coder avec Python <131595937+coder-avec-python@users.noreply.github.com>
2023-05-31 18:42:40 +0200

    Add files via upload

--- client/views.py
+++ client/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Client
+from commande.filters import CommandeFiltre
 
 # Create your views here.
 
@@ -9,5 +10,8 @@ def client(request, pk):
     client = Client.objects.get(id=pk)
     commande = client.commande_set.all()
     commande_total = commande.count()
-    context = {'client': client, 'commande': commande, 'commande_total': commande_total}
+    myfilter = CommandeFiltre(request.GET, queryset=commande)
+    commande = myfilter.qs
+
+    context = {'client': client, 'commande': commande, 'commande_total': commande_total, 'myfilter': myfilter}
     return render(request, "client/index.html", context)
